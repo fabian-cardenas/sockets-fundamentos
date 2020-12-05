1c27848daf1c4692030e0a3052e78aa86d717981
Fabian <fabian.ecm96@gmail.com>
2020-12-05 20:35:16 -0300

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,24 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+
+// Escuchar
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    // usuario: 'Fernando',
+    mensaje: 'Hola mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Esuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servido', mensaje);
+});
